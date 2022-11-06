a312c1aaf1424834858ceb002580f79fc41f72da
Arzu <rzaeva-010203@mail.ru>
2022-11-06 23:58:16 +0500

    Add DishOrders, table on OrderingIngredientsPage
    Добавила класс DishOrders для составления таблицы на экране "заказ продуктов" - допускается вариант, что класс будет заменен другим или api методом. Добавлена разметка таблицы на странице OrderingIngredientsPage и отображение экспериментальных данных.

--- Desktop-Canteen/ViewModels/OrderingIngredientsVM.cs
+++ Desktop-Canteen/ViewModels/OrderingIngredientsVM.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Windows.Documents;
 using WPFLibrary;
 using WPFLibrary.JsonModels;
 using WPFLibrary.Models;
@@ -10,19 +11,30 @@ namespace Desktop_Canteen.ViewModels;
 public class OrderingIngredientsVM : BaseVM
 {
     public ObservableCollection<SummaryOrderView> SummaryOrderViews { get; set; }
-    
+    //experimental format
+    public ObservableCollection<DishOrder> DishOrders { get; set; }
 
     public OrderingIngredientsVM()
     {
-        SummaryOrderViews = new ObservableCollection<SummaryOrderView>(ApiServer.Get<List<SummaryOrderView>>("SummaryOrderView"));
-        // Заглушка, не суди строго
-        // SummaryOrderViews = new ObservableCollection<SummaryOrderView>();
-        // var s = new SummaryOrderView();
-        // s.Date = DateTime.Today;
-        // s.Dish = new DishView
-        // {
-        //     Name = "Каша"
-        // };
-        // SummaryOrderViews.Add(s);
+        //SummaryOrderViews = new ObservableCollection<SummaryOrderView>(ApiServer.Get<List<SummaryOrderView>>("SummaryOrderView"));
+        DishOrders = new ObservableCollection<DishOrder>();
+        var row = new DishOrder(
+            new Dish
+            {
+                Name = "Каша овсяная"
+            },
+            511,
+            new List<Ingredient>
+            {
+                new Ingredient { Name = "Крупа овсяная", Measure = "г", Quantity = 100 },
+                new Ingredient { Name = "Молоко", Measure = "мл", Quantity = 100 }
+            },
+            new List<double>
+            {
+                1.0,
+                0.5
+            }
+        );
+        DishOrders.Add(row);
     }
 }
