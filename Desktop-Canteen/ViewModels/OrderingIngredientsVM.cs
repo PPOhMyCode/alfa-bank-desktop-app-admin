8548ed486dab5f72c702d3aa97f9e53861a3df51
Arzu <rzaeva-010203@mail.ru>
2022-11-07 14:19:03 +0500

    Change ordering table to new data type

--- Desktop-Canteen/ViewModels/OrderingIngredientsVM.cs
+++ Desktop-Canteen/ViewModels/OrderingIngredientsVM.cs
@@ -11,10 +11,25 @@ namespace Desktop_Canteen.ViewModels;
 public class OrderingIngredientsVM : BaseVM
 {
     public ObservableCollection<DishIngredientView> SummaryOrderViews { get; set; }
-    
-    public ObservableCollection<SummaryOrderDateIngrediets> ListOrders { get; set; }
+    public string SelectedDate { get; set; }
+
+    public List<List<string>> Values { get; set; }
     public OrderingIngredientsVM()
     {
-        SummaryOrderViews = new ObservableCollection<DishIngredientView>(ApiServer.Get<List<DishIngredientView>>("Orders/Date/11-28-2022/Ingredients"));
+        SelectedDate = "11-28-2022";
+        SummaryOrderViews = new ObservableCollection<DishIngredientView>(ApiServer.Get<List<DishIngredientView>>("Orders/Date/"+ SelectedDate +"/Ingredients"));
+
+        Values = new List<List<string>>();
+        foreach (var summaryOrderView in SummaryOrderViews)
+        {
+            var countOrders = summaryOrderView.CountOrders;
+            var resultList = new List<string>();
+            foreach (var ingredientCount in summaryOrderView.Ingredients)
+            {
+                resultList.Add((ingredientCount.Count * ingredientCount.Ingredient.Quantity * countOrders).ToString() + " " +
+                          ingredientCount.Ingredient.Measure);
+            }
+            Values.Add(resultList);
+        }
     }
 }
