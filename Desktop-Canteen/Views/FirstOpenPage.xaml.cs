3799caf9d8d8811a74e99dc0853e4ae48300f72d
Arzu <rzaeva-010203@mail.ru>
2023-04-19 23:05:06 +0500

    Новая стартовая страница в приложении работника столовой

---
+++ Desktop-Canteen/Views/FirstOpenPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Desktop_Canteen.Views;
+
+public partial class FirstOpenPage : Page
+{
+    public FirstOpenPage()
+    {
+        InitializeComponent();
+    }
+    
+    public void StartButtonClick(object sender, RoutedEventArgs e)
+    {
+        PlugPanel.Visibility = Visibility.Hidden;
+        InstructionsGrid.Visibility = Visibility.Visible;
+        DatesPanel.Visibility = Visibility.Visible;
+        PeriodPanel.Visibility = Visibility.Visible;
+    }
+    
+    public void SaveButtonClick(object sender, RoutedEventArgs e)
+    {
+        NavigationService?.Navigate(new MenuPage());
+    }
+}
